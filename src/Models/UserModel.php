efb406495debd47a682a0e19b4f680597f81e786
Claudia Lim <climweiching@gmail.com>
2024-03-14 12:36:59 +0000

    get functions in user model

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -10,16 +10,16 @@ class UserModel
         $this->db = $db;
     }
 
-    public function getUsernameById(int $id) {
-        $query = $this->db->prepare("SELECT `username` FROM `users` WHERE `id` = ?");
+    public function getUserInfoById(int $id) {
+        $query = $this->db->prepare("SELECT `username`, `password`, `email` FROM `users` WHERE `id` = ?");
         $query->execute([$id]);
         return $query->fetch();
     }
 
     public function getUserIdByUsernameOrEmail(string $username, string $email) {
         $query = $this->db->prepare("SELECT `id` FROM `users` WHERE `username` = :username OR `email` = :email");
-        $query->bindParam(':username', $username);
-        $query->bindParam(':email', $email);
+        $query->bindParam(":username", $username);
+        $query->bindParam(":email", $username);
         $query->execute();
         return $query->fetch();
     }
